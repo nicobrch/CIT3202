57cc13bb2c7c895cd971a90cb90796aeaa59fa2a
Nico <nicolas.brch@gmail.com>
2024-05-01 23:14:06 -0400

    Cambiar configuración en un archivo aparte

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
+import config
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-llm = Ollama(model="llama3")
+llm = Ollama(model="llama3", temperature=0.3)
 
-prompt = ChatPromptTemplate.from_messages([
-  ("system", "Eres Leyla, una servicial asistente virtual para la empresa Ripley, de Chile. Tu trabajo será ayudar a los usuarios con sus dudas y preguntas respecto a productos, servicios y promociones de la empresa. No respondas nada fuera de este contexto."),
-  ("user", "{input}")
-])
+prompt = ChatPromptTemplate.from_template(config.system_prompt)
 
 output_parser = StrOutputParser()
 
 chain = prompt | llm | output_parser
 
-print(chain.invoke({"input": "¿Cuál es el horario de atención de Ripley?"}))
+response = chain.invoke({"input": "Tienen figuras de one piece?"})
+
+print("Geeki: ", response)
---
+++ config.py
@@ -0,0 +1,10 @@
+# Prompt inicial para el bot de Geekz
+system_prompt = """system: Eres Geeki, una servicial asistente virtual para la empresa Geekz, de Chile. Tu trabajo será ayudar a los usuarios con sus dudas y preguntas respecto a productos, servicios y promociones de la empresa. No respondas nada fuera de este contexto. Mantén tus respuestas breves.
+
+user: {input}
+"""
+
+# Páginas de la empresa Geekz para el WebBaseLoader
+web_paths = [
+  "https://geekz.cl/preguntas-frecuentes",
+]
