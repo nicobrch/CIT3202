47baa09b74b198e50011dce20e54fa6aec9ef177
Nico <nicolas.brch@gmail.com>
2024-05-05 19:00:59 -0400

    Obtener valores de archivo TOML

--- config.py
+++ config.py
@@ -1,14 +1,3 @@
-# Prompt inicial para el bot de Geekz
-system_prompt = """system: Eres Geeki, una servicial asistente virtual para la empresa Geekz, de Chile. Tu trabajo será ayudar a los usuarios con sus dudas y preguntas respecto a productos, servicios y promociones de la empresa. No respondas nada fuera de este contexto. Mantén tus respuestas breves.
-
-user: {input}
-"""
-
-# Páginas de la empresa Geekz para el WebBaseLoader
-web_paths = [
-  "https://geekz.cl/preguntas-frecuentes",
-]
-
 # Configuración de credenciales
 import toml
 
@@ -16,3 +5,11 @@ def load_secrets():
   with open("secrets.toml", "r") as file:
     secrets = toml.load(file)
   return secrets
+
+secrets = load_secrets()
+
+# Prompt inicial para el bot de Geekz
+system_prompt = secrets["chatbot"]["system_prompt"]
+
+# Páginas de la empresa Geekz
+web_product_path = secrets["chatbot"]["web_product_path"]
