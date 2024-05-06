267d079137679b9601fc0db00a1c692bb1f556e4
Nico <nicolas.brch@gmail.com>
2024-05-06 13:46:04 -0400

    Obtener documentos HTML de la página

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # Configuración de credenciales
 import toml
+import os
+import requests
+from bs4 import BeautifulSoup
 
 def load_secrets():
   with open("secrets.toml", "r") as file:
@@ -15,4 +18,21 @@ api_key = secrets["credentials"]["openai_api_key"]
 system_prompt = secrets["chatbot"]["system_prompt"]
 
 # Páginas de la empresa Geekz
-web_product_path = secrets["chatbot"]["web_product_path"]
+web_products = secrets["web"]["products"]
+
+web_documents = secrets["web"]["documents"]
+
+def download_html(url, directory):
+  response = requests.get(url)
+  
+  if response.status_code == 200:
+      soup = BeautifulSoup(response.content, 'html.parser')
+      
+      filename = os.path.join(directory, url.split('/')[-1] + ".html")
+      
+      with open(filename, 'w', encoding='utf-8') as file:
+          file.write(str(soup))
+      
+      print(f"HTML file saved: {filename}")
+  else:
+      print(f"Failed to download HTML content from {url}")
