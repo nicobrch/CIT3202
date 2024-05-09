6507cd2176135abb981f65f0f33a51a828eca8d8
Nico <nicolas.brch@gmail.com>
2024-05-09 15:29:04 -0400

    Modificar configuracion

--- src/config.py
+++ src/config.py
@@ -1,8 +1,5 @@
 # Configuración de credenciales
 import toml
-import os
-import requests
-from bs4 import BeautifulSoup
 
 def load_secrets():
   with open("secrets.toml", "r") as file:
@@ -20,19 +17,5 @@ system_prompt = secrets["chatbot"]["system_prompt"]
 # Páginas de la empresa Geekz
 web_products = secrets["web"]["products"]
 
+# Documentos de la empresa Geekz
 web_documents = secrets["web"]["documents"]
-
-def download_html(url, directory):
-  response = requests.get(url)
-  
-  if response.status_code == 200:
-      soup = BeautifulSoup(response.content, 'html.parser')
-      
-      filename = os.path.join(directory, url.split('/')[-1] + ".html")
-      
-      with open(filename, 'w', encoding='utf-8') as file:
-          file.write(str(soup))
-      
-      print(f"HTML file saved: {filename}")
-  else:
-      print(f"Failed to download HTML content from {url}")
