160f392c127a4110b7c2901a2ea72675db10cc8b
Nico <nicolas.brch@gmail.com>
2024-05-31 12:48:27 -0400

    Agregar streaming a respuestas

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,12 +1,23 @@
-import bot_tools
 import os
+import db
+import streamlit as st
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.agents import create_tool_calling_agent, AgentExecutor
-from langchain_core.messages import HumanMessage
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.callbacks.base import BaseCallbackHandler
 
 load_dotenv()
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
 
 llm = ChatOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
@@ -15,12 +26,13 @@ llm = ChatOpenAI(
     streaming=True,
 )
 
-tools = bot_tools.tools
+tools = [
+    db.similarity_search,
+    db.search_products,
+    db.cheaper_products,
+]
 
-llm_with_tools = llm.bind_tools(tools)
-
-system_prompt = """Eres Geeki, una servicial asistente virtual para la empresa \"Geekz\", de Chile. Geekz es una tienda virtual \"https://geekz.cl/\" y física que vende figuras coleccionables y juguetes nerds, tales como Funko POP, Pokemon, Nintendo, Marvel y muchos otros.
-Tu trabajo será ayudar a los usuarios con sus dudas y preguntas respecto a productos, servicios e información de la empresa, utilizando las herramientas necesarias.
+system_prompt = """Eres Geeki, una servicial asistente virtual para la empresa \"Geekz\", de Chile. Geekz es una tienda virtual \"https://geekz.cl/\" y física que vende figuras coleccionables y juguetes nerds, tales como Funko POP, Pokemon, Nintendo, Marvel y muchos otros. Tu trabajo será ayudar a los usuarios con sus dudas y preguntas respecto a productos, servicios e información de la empresa, utilizando las herramientas necesarias. Los precios de productos están en CLP.
 No respondas preguntas que no sean respecto a Geekz, en ese caso responde amablemente que no puedes responder dicha información. Bajo ninguna circunstancia inventes información. Mantén tus respuestas breves y acorde a la conversación del chat. Al responder, pregunta si necesita ayuda adicional o, si busca productos, sugiérele productos relacionados."""
 
 prompt = ChatPromptTemplate.from_messages(
@@ -32,35 +44,32 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-agent = create_tool_calling_agent(
-    llm=llm_with_tools,
-    prompt=prompt,
-    tools=tools,
-)
+agent = create_openai_tools_agent(llm.with_config({"tags": ["agent_llm"]}), tools, prompt)
 
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True,
-    handle_parsing_errors=True
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True).with_config(
+    {"run_name": "Agent"}
 )
 
 chat_history = []
 
-def ask(prompt, callbacks):
-    response = agent.invoke(
+def ask(prompt):
+    stream_handler = StreamHandler(st.empty())
+    llm.callbacks = [stream_handler]
+    
+    response = agent_executor.invoke(
         {
-            "chat_history": chat_history,
             "input": prompt,
-        },
-        {
-            "callbacks": [callbacks]
+            "chat_history": chat_history,
         }
     )
 
     output = response["output"]
 
-    chat_history.extend([HumanMessage(content=prompt), output])
+    chat_history.extend([
+        HumanMessage(content=prompt),
+        AIMessage(content=output)
+    ])
+
     while len(chat_history) > 3:
         chat_history.pop(0)
 
