3b3ed339f769eca6982ba1c9785cbaa98fd00a35
Nico <nicolas.brch@gmail.com>
2024-05-31 12:48:54 -0400

    Agrear try except a queries, agregar query para productos baratos

--- src/db.py
+++ src/db.py
@@ -12,14 +12,36 @@ load_dotenv()
 @tool
 def similarity_search(query: str):
     """Busca información de la empresa, tales como horarios de atención tiendas y servicio al cliente, preguntas frecuentes, políticas de privacidad, despacho, devoluciones, etc. Recibe un texto de entrada para realizar una búsqueda por similitud."""
-    matched_docs = vectorstore.similarity_search(query)
-    return matched_docs[0].page_content
+    try:
+        matched_docs = vectorstore.similarity_search(query)
+        return matched_docs[0].page_content
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 @tool
 def search_products(name: str):
     """Busca productos por su nombre."""
-    data, count = supabase.table("products").select("*").ilike("name", f"%{name}%").execute()
-    return data
+    try:
+        data, count = supabase.table("products").select("*").ilike("name", f"%{name}%").execute()
+        return data
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+@tool
+def cheaper_products(quantity: int, name: str = None):
+    """Busca los N productos más baratos. Recibe un número entero para la cantidad de productos a buscar. También puede recibir un nombre de producto para buscar productos más baratos que contengan dicho nombre."""
+    try:
+        if name:
+            data, count = supabase.table("products").select("*").ilike("name", f"%{name}%").limit(quantity).order("price").execute()
+            return data
+        else:
+            data, count = supabase.table("products").select("*").limit(quantity).order("price").execute()
+            return data
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_SERVICE_KEY"))
 
