55f58256e1a495437ef6855b669ca64ab7b33dc0
Nico <nicolas.brch@gmail.com>
2024-07-01 01:17:35 -0400

    Intento de fine tuning 1

--- db/parse_jsonl.py
+++ db/parse_jsonl.py
@@ -6,10 +6,10 @@ def json_to_jsonl(json_file, jsonl_file):
     
     with open(jsonl_file, 'w', encoding='utf-8') as f:
         for entry in data:
-            json_line = json.dumps(entry)
+            json_line = json.dumps(entry, ensure_ascii=False)
             f.write(json_line + '\n')
 
 # Example usage
-json_file = 'tuning.json'
-jsonl_file = 'finetuning.jsonl'
+json_file = './db/tuning.json'
+jsonl_file = './db/finetuning.jsonl'
 json_to_jsonl(json_file, jsonl_file)
